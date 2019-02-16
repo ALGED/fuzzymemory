8bd27785d55f1e6520b88cc41f0299c8e4f5a1f4
Andrdragon] <andrdragon@gmail.com>
2019-02-16 17:56:18 +0200

    third commit

--- JS/script.js
+++ JS/script.js
@@ -1,1 +1,11 @@
-alert(true);
+var string = "Lore4m ipsum d6olor sit amet, consectetur adipisicing elit. Nisi distinctio hic eveniet magnam nostrum, vel maiores repudiandae dolorum eligendi eius aspernatur repellendus perferendis, tenetur libero. Eligendi ipsa, voluptatem libero ut."
+var searchString = "em"
+var sum = 0;
+for (var i = 0; i < string.length; i++) {
+	if (string(i) ==) {}
+}
+console.log(string.
+/*console.log(string.substring(2,5))*/
+/*console.log(string.indexOf("x"));*/
+/*console.log(string.substr(0,3))*/ // start, length
+/*console.log(string.slice())*/ //start, end
